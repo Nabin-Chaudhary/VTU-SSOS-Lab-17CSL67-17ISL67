4323c3d0ee4e134e15a767b234f3a760ce8463dd
Nabin-Chaudhary <nabin.clnc@gmail.com>
2020-11-16 16:21:10 +0545

    Add files via upload

---
+++ LEX & YACC Part/6th program/a/file1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+//declaration
+int a=4, b=8;
+/*printing statements*/
+printf("Hello");
+scanf("%d%d",&a,&b);
+/*Very simple demo 
+program simply*/
+getch();
+}
---
+++ LEX & YACC Part/6th program/b/Nabin.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a=5, b=5, c;
+c=a*b;
+printf("Multiplication = %d",c);
+if(c = 25)
+printf("Correct Answer Nabin, Thanks to Mam");
+else
+printf("Not correct, so sad::try again");
+}
